d1e89845047bdd0aec47e5c7d579752a8f3402ef
Alexandre MARTY <alexandre.marty@ymail.com>
2016-04-01 15:34:35 +0200

    créé un package de l'équipe

---
+++ PRIME_CACAO_2016/Main.java
@@ -0,0 +1,13 @@
+package prime2016;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("(:");
+		System.out.println("Octopusses Are Cool *u*");
+		System.out.println("(:");
+		System.out.println("One day they'll fly and invade our world ! They'll build floating cities in the dusklight-bathed skies.");
+	}
+
+}
