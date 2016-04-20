394dcbfbe7590649aa8bb54fa7a0658e4d06aea3
Manette <Manette@172.18.104.104>
2016-04-20 16:00:49 +0200

    Squelette des premières méthodes

--- PRIME_CACAO_2016/src/abstraction/equipe1/Producteur.java
+++ PRIME_CACAO_2016/src/abstraction/equipe1/Producteur.java
@@ -1,6 +1,29 @@
 package abstraction.equipe1;
 
-public class Producteur {
-	private int stock;
-	private int tresorerie;
+import abstraction.fourni.Acteur;
+
+public class Producteur implements Acteur {//, IProducteur {
+	private String nom;
+	private double stock;
+	private double tresorerie;
+	
+	public Producteur(String nom, double stock_initial, double treso_initiale) {
+		this.nom = nom;
+		this.stock = stock_initial;
+		this.tresorerie = treso_initiale;
+	}
+	
+	private double productionInstantanee() {
+		// TODO !
+		return 0.0;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public void next() {
+		// TODO Auto-generated method stub
+		
+	}
 }
