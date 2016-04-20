8c38d7f075b2957ca28e601429521f1412e33d79
Marc <marco.suteau@gmail.com>
2016-04-20 16:12:00 +0200

    création d'un setter pour la commande + création fonction commande à appeler avant passage au step suivant

--- PRIME_CACAO_2016/src/abstraction/equipe3/ActeurLeclerc.java
+++ PRIME_CACAO_2016/src/abstraction/equipe3/ActeurLeclerc.java
@@ -1,9 +1,36 @@
 package abstraction.equipe3;
 
+import abstraction.fourni.Indicateur;
+
 public class ActeurLeclerc {
+	
+	private String nom;
+	private double quantite;
+	private double prix;
+	private Indicateur solde;
+	private Indicateur achats;
+	
 
 	public ActeurLeclerc() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void setQte(double commande){
+		this.quantite=commande;
+	}
+	
+	public void commande(int step){
+		if (step==4){
+			setQte(3812.5);
+		}
+		else{
+			if (step==21){
+				setQte(6312.5);
+			}
+			else{
+				setQte(1812.5);
+			}
+		}
 	}
 
 }
