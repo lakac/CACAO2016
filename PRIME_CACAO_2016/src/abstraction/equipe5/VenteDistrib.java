69b82b81b0549f2ddbe0f2d8904c483b75b813aa
May-linh <may.linh@live.fr>
2016-04-27 12:10:33 +0200

    en cours de changement de vente distrib

--- PRIME_CACAO_2016/src/abstraction/equipe5/VenteDistrib.java
+++ PRIME_CACAO_2016/src/abstraction/equipe5/VenteDistrib.java
@@ -1,5 +1,23 @@
 package abstraction.equipe5;
 
+import abstraction.commun.Constantes;
+import abstraction.commun.ITransformateur;
+import abstraction.commun.MondeV1;
+
 public class VenteDistrib {
+	private Historique_Commande_Dist hist;
+	
+	
+	public VenteDistrib(Historique_Commande_Dist hist){
+		this.hist=hist;
+	}
+	
+	public double getDemande(ITransformateur t){
+		if (t==MondeV1.LE_MONDE.getActeur(Constantes.NOM_PRODUCTEUR_1)){
+			return (0);
+		}
+		return 0.6*hist.valeur(Historique_Commande_Dist.STEP_COURANT);
+	}
+
 
 }
