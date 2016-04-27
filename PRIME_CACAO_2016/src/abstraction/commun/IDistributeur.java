f1e19af9eb7183f78d1756c2780a216649def765
JMagloire <JMag@dhcp-4-89.invite.emn.fr>
2016-04-27 11:14:00 +0200

    modification de IDitributeur (vu avec les distributeurs)

--- PRIME_CACAO_2016/src/abstraction/commun/IDistributeur.java
+++ PRIME_CACAO_2016/src/abstraction/commun/IDistributeur.java
@@ -1,6 +1,6 @@
 package abstraction.commun;
 
 public interface IDistributeur {
-	public double getDemande(ITransformateur t, int step);
+	public double getDemande(ITransformateur t);
 	public double getPrix();
 }
