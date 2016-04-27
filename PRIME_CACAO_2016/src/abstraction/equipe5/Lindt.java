ce701a5dc49b59b2f5b115946f12f538d5b34f85
JMagloire <JMag@dhcp-4-89.invite.emn.fr>
2016-04-27 21:08:33 +0200

    ajout d'un commentaire dans Lindt

--- PRIME_CACAO_2016/src/abstraction/equipe5/Lindt.java
+++ PRIME_CACAO_2016/src/abstraction/equipe5/Lindt.java
@@ -71,7 +71,7 @@ public class Lindt implements Acteur, ITransformateur{
 	 * 
 	 * Cette méthode est appelée par les producteurs.
 	 */
-	public void notificationVente(IProducteur p){
+	public void notificationVente(IProducteur p){ // on travaille avec chaque producteur d'où le ratio de 0.3 à chaque fois
 		stock_cacao.ajouterStock(0.3 * Constante.RATIO_CACAO_CHOCOLAT * hist.valeur(Constante.STEP_PRECEDENT));
 		stock_cacao.retirerStock(0.3 * Constante.RATIO_CACAO_CHOCOLAT * hist.valeur(Constante.STEP_PRECEDENT_MOINS_2));
 		treso.retirerTresorerie(0.3 * Constante.RATIO_CACAO_CHOCOLAT * hist.valeur(Constante.STEP_PRECEDENT) * p.annoncePrix());
