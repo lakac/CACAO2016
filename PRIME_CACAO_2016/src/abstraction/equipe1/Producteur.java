1ccf904b00072147033f164b2d98ea29d083a885
Manette <Manette@172.18.121.162>
2016-04-27 09:20:57 +0200

    Implémentation de la méthode next() de abstraction.equipe1.Producteur, ainsi que d'une méthode annexe getTransformateurs() et mise à jour de la méthode produire() pour qu'elle modifie la quantité proposée à chaque transformateur à chaque step.

--- PRIME_CACAO_2016/src/abstraction/equipe1/Producteur.java
+++ PRIME_CACAO_2016/src/abstraction/equipe1/Producteur.java
@@ -8,6 +8,8 @@ import abstraction.commun.IProducteur;
 import abstraction.commun.ITransformateur;
 import abstraction.commun.Constantes;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -18,16 +20,12 @@ public class Producteur implements Acteur, IProducteur {
 	private double prixVente;
 	private double productionAnnuelle;
 	private double[] productionDeBase;
-	private Map<String,Double> quantitesProposees;
+	private Map<ITransformateur,Double> quantitesProposees;
 	private Indicateur productionCourante;
 	private Journal journal;
 	
 	/**
-	 * Initialise notre producteur a partir d'un stock et d'une tresorerie initiaux
-	 * @param nom
-	 * @param stock_initial
-	 * @param treso_initiale
-	 * @param monde
+	 * Initialise notre producteur a partir d'un stock et d'une tresorerie initiaux.
 	 */
 	public Producteur(String nom, double stockInitial, double tresoInitiale, Monde monde) {
 		this.nom = nom;
@@ -44,23 +42,27 @@ public class Producteur implements Acteur, IProducteur {
 		this.productionDeBase = new double[] {0.008, 0.012, 0.014, 0.017, 0.028, 0.03, 0.017, 0.014, 0.034, 0.047, 0.112,
 			0.111, 0.044, 0.036, 0.017, 0.016, 0.035, 0.043, 0.111, 0.112, 0.048, 0.035, 0.024, 0.015, 0.012, 0.008}; 
 		
-		this.quantitesProposees = new HashMap<String,Double>();
-		this.quantitesProposees.put(Constantes.NOM_TRANSFORMATEUR_1,0.0);
-		this.quantitesProposees.put(Constantes.NOM_TRANSFORMATEUR_2,0.0);
-		this.quantitesProposees.put("Autres",0.0);
+		this.quantitesProposees = new HashMap<ITransformateur,Double>();
+		for (ITransformateur t : this.getTransformateurs()) {
+			this.quantitesProposees.put(t,0.0);
+		}
 		
 		this.journal = new Journal("Journal de "+this.nom);
 		Monde.LE_MONDE.ajouterJournal(this.journal);
 	}
 	
 	/**
-	 * @param step
-	 * @return la quantite de cacao produite au step actuel
+	 * @return la quantite de cacao produite au step actuel.
 	 * 
 	 * Plus tard, on ajoutera une fluctuation autour de la quantite de base.
 	 */
 	private void produire(int step) {
 		this.productionCourante.setValeur(this, this.getProductionDeBase(step)*this.getProductionAnnuelle());
+		this.setStock(this.getStock()+this.productionCourante.getValeur());
+		for (ITransformateur t : this.getTransformateurs()) {
+			this.quantitesProposees.put(t,this.getProductionCourante().getValeur()*0.5);
+			// Pour l'instant, on a seulement deux clients
+		}
 	}
 	
 	public String getNom() {
@@ -96,8 +98,7 @@ public class Producteur implements Acteur, IProducteur {
 	}
 	
 	/**
-	 * @param step
-	 * @return la production de base de notre producteur a la periode de l'annee correspondant au step actuel
+	 * @return la production de base de notre producteur a la periode de l'annee correspondant au step actuel.
 	 */
 	private double getProductionDeBase(int step) {
 		return this.productionDeBase[step%26];
@@ -112,22 +113,21 @@ public class Producteur implements Acteur, IProducteur {
 	}
 	
 	/**
-	 * Doublon avec getQuantiteProposee... Simplifier avec les conventions de nommage
+	 * Doublon avec getQuantiteProposee... Simplifier avec les conventions de nommage !
 	 */
 	public double annonceQuantiteMiseEnVente(ITransformateur t) {
 		return this.getQuantiteProposee(t);
 	}
 	
 	/**
-	 * Doublon avec getPrixVente... Simplifier avec les conventions de nommage
+	 * Doublon avec getPrixVente... Simplifier avec les conventions de nommage !
 	 */
 	public double annoncePrix() {
 		return this.getPrixVente();
 	}
 	
 	/**
-	 * Met a jour les stocks et tresoreries du transformateur t et de ce producteur
-	 * @param t
+	 * Met a jour les stocks et tresoreries du transformateur t et de notre producteur.
 	 */
 	private void notificationVente(ITransformateur t) {
 		double quantiteVendue = Math.min(t.annonceQuantiteDemandee(this), this.annonceQuantiteMiseEnVente(t));
@@ -135,22 +135,32 @@ public class Producteur implements Acteur, IProducteur {
 		this.setStock(this.getStock() - quantiteVendue);
 		this.setTresorerie(this.getTresorerie() + quantiteVendue*this.getPrixVente());
 		t.notificationVente(this);
+		// Il faudra que les transformateurs aient bien acces a la quantite reellement vendue...
+	}
+	
+	private List<ITransformateur> getTransformateurs() {
+		List<ITransformateur> transfos = new ArrayList<ITransformateur>();
+		for (Acteur a : Monde.LE_MONDE.getActeurs()) {
+			if (a instanceof ITransformateur) {
+				transfos.add((ITransformateur)(a));
+			}
+		}
+		return transfos;
 	}
 	
 	public void next() {
-		// TODO Auto-generated method stub
-		
-		// Partie 1 : Transfo.notificationVente() pour T1 et T2
+		// Partie 1 : Gestion des ventes (Transfo.notificationVente() pour T1 et T2)
+		for (ITransformateur t : this.getTransformateurs()) {
+			this.notificationVente(t);
+		}
 		
 		// Partie 2 : mise a jour de la quantite disponible a la vente et du prix de vente du prochain step
 		// pour que les transfos y aient acces
 		
-		produire(Monde.LE_MONDE.getStep());
-		if (Monde.LE_MONDE.getStep()%3==0) {
-			this.journal.ajouter("Production de "+this.getNom()+" = <font color=\"maroon\">"+this.getProductionCourante()+"</font> au <b>step</b> "+Monde.LE_MONDE.getStep());
-		}
+		this.produire(Monde.LE_MONDE.getStep());
+		this.journal.ajouter("Production de "+this.getNom()+" = <font color=\"maroon\">"+this.getProductionCourante().getValeur()+"</font> au <b>step</b> "+Monde.LE_MONDE.getStep());
 		
 		// Plus tard, on ajoutera des fluctuations dans le prix de vente ; pour l'instant il est a 3000 euros par tonne
-		// setPrixVente
+		// [setPrixVente]
 	}
 }
