003dffda5abe75584b0fdf5c161c58776ea8f3a8
Sofiane <salort.sofiane@gmail.com>
2016-04-27 16:48:03 +0200

    ajout de t1 et Ca aux acteurs du monde dans Mondev1

--- PRIME_CACAO_2016/src/abstraction/commun/MondeV1.java
+++ PRIME_CACAO_2016/src/abstraction/commun/MondeV1.java
@@ -14,9 +14,11 @@ public class MondeV1 extends Monde {
 		// Il faut créer les acteurs et les ajouter au monde ici.
 		// Distributeurs
 		Carrefour Ca = new Carrefour("Carrefour", this, 15, 20, 50000);
+		this.ajouterActeur(Ca);
 		// Transformateurs
 		Transformateur2 t1 = new Transformateur2(Constantes.NOM_TRANSFORMATEUR_1, this);
-
+		
+		this.ajouterActeur(t1);
 		// Producteurs
 
 		Producteur p1 = new Producteur(Constantes.NOM_PRODUCTEUR_1, 1000.0, 0.0, Monde.LE_MONDE);
