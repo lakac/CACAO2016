f788c3e9d7bbc0864098d9264d821eb0dad4e7fc
Floriane Dahman <floriane.dahman@hotmail.fr>
2016-04-27 19:36:34 +0200

    Création fonction demandeTotale

--- PRIME_CACAO_2016/src/abstraction/equipe5/VenteDistrib.java
+++ PRIME_CACAO_2016/src/abstraction/equipe5/VenteDistrib.java
@@ -14,17 +14,8 @@ public class VenteDistrib {
 		this.quantiteMiseEnVente=q;
 	}
 	
-	public double getDemande(ITransformateur t){
-		double quantiteTotale = 
-		if (t==MondeV1.LE_MONDE.getActeur(Constantes.NOM_TRANSFORMATEUR_1)){
-			return (0);
-		}
-		else{
-			if (t==MondeV1.LE_MONDE.getActeur(Constantes.NOM_TRANSFORMATEUR_2)){
-				return (0.3*Math.min(quantiteTotale, this.quantiteMiseEnVente));
-			}
-			else{
-				return 0;
-			}
+	public double getDemandeTotale(){
+		return (getDemande((ITransformateur))+getDemande(ITransformateur));
+
 
 }}
