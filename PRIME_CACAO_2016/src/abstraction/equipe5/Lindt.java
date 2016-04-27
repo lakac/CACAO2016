a8f7835b089b7400278a465a019a4d6a0405f1a6
JMagloire <JMag@dhcp-4-89.invite.emn.fr>
2016-04-27 21:03:55 +0200

    Modif Tresorerie et Lindt

--- PRIME_CACAO_2016/src/abstraction/equipe5/Lindt.java
+++ PRIME_CACAO_2016/src/abstraction/equipe5/Lindt.java
@@ -55,14 +55,15 @@ public class Lindt implements Acteur, ITransformateur{
 		this.getHist().ajouter(D1.getDemande(this) + D2.getDemande(this));
 		stock_chocolat.ajouterStock(this.getHist().valeur(Constante.STEP_PRECEDENT_MOINS_2));
 		stock_chocolat.retirerStock(this.getHist().valeur(Constante.STEP_PRECEDENT_MOINS_3));
-		treso.ajouterTresorerie(this.getHist().valeur(Constante.STEP_PRECEDENT_MOINS_3)*15000);
+		stock_cacao.ajouterStock(0.4 * Constante.RATIO_CACAO_CHOCOLAT * hist.valeur(Constante.STEP_PRECEDENT)); // stock lié au reste du monde
+		stock_cacao.retirerStock(0.4 * Constante.RATIO_CACAO_CHOCOLAT * hist.valeur(Constante.STEP_PRECEDENT_MOINS_2)); // stock lié au reste du monde
+		treso.ajouterTresorerie(treso.marge());
+		treso.retirerTresorerie(0.3 * Constante.RATIO_CACAO_CHOCOLAT * hist.valeur(Constante.STEP_PRECEDENT) * 3000); // achat cacao au reste du monde
 		
 		this.etatStockCacao.setValeur(this, this.stock_cacao.getStock());
 		this.etatStockChocolat.setValeur(this, this.stock_chocolat.getStock());
 		this.venteChocolat.setValeur(this, this.stock_chocolat.getStock());	
 	}
-	
-
 
 	/**
 	 * Met à jour l'état interne de ce transformateur
@@ -71,9 +72,9 @@ public class Lindt implements Acteur, ITransformateur{
 	 * Cette méthode est appelée par les producteurs.
 	 */
 	public void notificationVente(IProducteur p){
-		stock_cacao.ajouterStock(Constante.RATIO_CACAO_CHOCOLAT*hist.valeur(Constante.STEP_PRECEDENT));
-		stock_cacao.retirerStock(Constante.RATIO_CACAO_CHOCOLAT*hist.valeur(Constante.STEP_PRECEDENT_MOINS_2));
-		treso.retirerTresorerie(p.annoncePrix());
+		stock_cacao.ajouterStock(0.3 * Constante.RATIO_CACAO_CHOCOLAT * hist.valeur(Constante.STEP_PRECEDENT));
+		stock_cacao.retirerStock(0.3 * Constante.RATIO_CACAO_CHOCOLAT * hist.valeur(Constante.STEP_PRECEDENT_MOINS_2));
+		treso.retirerTresorerie(0.3 * Constante.RATIO_CACAO_CHOCOLAT * hist.valeur(Constante.STEP_PRECEDENT) * p.annoncePrix());
 		System.out.println("Met à jour le stock et la tréso");
 	}
 
