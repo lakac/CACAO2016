e1e07d1fd3a38f43dfb6c0c0344d49e505b067fc
Floriane Dahman <floriane.dahman@hotmail.fr>
2016-04-27 12:09:47 +0200

    Création de la fonction de AchatProd

--- PRIME_CACAO_2016/src/abstraction/equipe5/AchatProd.java
+++ PRIME_CACAO_2016/src/abstraction/equipe5/AchatProd.java
@@ -6,19 +6,28 @@ import abstraction.commun.MondeV1;
 
 public class AchatProd {
 	private Historique_Commande_Dist hist;
+	private double quantiteMiseEnVente;
 	
 	
-	public AchatProd(Historique_Commande_Dist hist){
+	public AchatProd(Historique_Commande_Dist hist, double q){
 		this.hist=hist;
+		this.quantiteMiseEnVente=q;
 	}
 	/**
 	 * Indique la quantité demandée au producteur p.
 	 */
 	public double annonceQuantiteDemandee(IProducteur p){
+		double quantiteTotale = 0.6*hist.valeur(Historique_Commande_Dist.STEP_COURANT);
 		if (p==MondeV1.LE_MONDE.getActeur(Constantes.NOM_PRODUCTEUR_1)){
-			return (0);
+			return (0.3*Math.min(quantiteTotale, this.quantiteMiseEnVente));
 		}
-		return 0.6*hist.valeur(Historique_Commande_Dist.STEP_COURANT);
-	}
+		else{
+			if (p==MondeV1.LE_MONDE.getActeur(Constantes.NOM_PRODUCTEUR_2)){
+				return (0.3*Math.min(quantiteTotale, this.quantiteMiseEnVente));
+			}
+			else{
+				return 0;
+			}
+	}}
 
 }
