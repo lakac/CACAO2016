bfe8c36cc5a484a0be4f0ce6ce42ce18dfc7776f
Benoît Schmitt <Benoit.schmitt@etudiant.mines-nantes.fr>
2016-04-29 16:15:06 +0200

    Tipographie

--- PRIME_CACAO_2016/src/abstraction/equipe4/Producteur.java
+++ PRIME_CACAO_2016/src/abstraction/equipe4/Producteur.java
@@ -1,4 +1,4 @@
-package abstraction.equipe4;
+package abstraction.equipe4;	
 import abstraction.fourni.*;
 import java.util.ArrayList;
 import abstraction.commun.*;
@@ -10,7 +10,7 @@ public class Producteur implements Acteur,IProducteur{
 	private double prod; //production semi annuelle
 	private Tresorerie treso;
 	private Indicateur pertes; //perte dans la production totale
-	private Indicateur prod_tot_utilisable; //production totale sans les pertes
+	private Indicateur prodUtilisable; //production totale sans les pertes
 	
 	private ArrayList<ITransformateur> transformateurs;
 	
@@ -20,7 +20,7 @@ public class Producteur implements Acteur,IProducteur{
        this.nom = Constantes.NOM_PRODUCTEUR_2;
 	   this.treso = new Tresorerie(this,3000.0,10.0);
 	   this.stock = new Indicateur("Stock de "+ this.nom,this,0.0);
-       this.prod_tot_utilisable = new Indicateur(Constantes.IND_PRODUCTION_P2, this, 0.0);
+       this.prodUtilisable = new Indicateur(Constantes.IND_PRODUCTION_P2, this, 0.0);
        this.journal = new Journal("Journal de "+this.nom);
        this.pertes = new Indicateur("Pertes de "+this.nom,this,0.0);
        this.prod=1200000;
@@ -30,7 +30,7 @@ public class Producteur implements Acteur,IProducteur{
 				this.transformateurs.add((ITransformateur)(a));
 			}
 		}       Monde.LE_MONDE.ajouterJournal(this.journal);
-    	Monde.LE_MONDE.ajouterIndicateur( this.prod_tot_utilisable );
+    	Monde.LE_MONDE.ajouterIndicateur( this.prodUtilisable );
     	Monde.LE_MONDE.ajouterIndicateur(this.pertes);
     	Monde.LE_MONDE.ajouterIndicateur(this.stock);
     }
@@ -60,9 +60,9 @@ public class Producteur implements Acteur,IProducteur{
 		
 		if (Monde.LE_MONDE.getStep()%12==1){ //production semi annuelle
 			this.pertes.setValeur(this, Math.random()*200000);
-			this.prod_tot_utilisable.setValeur(this,this.prod-this.pertes.getValeur());
-			this.Augment_Stock(this.prod_tot_utilisable.getValeur());
-			this.journal.ajouter("Production de semi annuelle de " + this.prod_tot_utilisable);
+			this.prodUtilisable.setValeur(this,this.prod-this.pertes.getValeur());
+			this.Augment_Stock(this.prodUtilisable.getValeur());
+			this.journal.ajouter("Production de semi annuelle de " + this.prodUtilisable);
 		}
 		if(Monde.LE_MONDE.getStep()==1){ //premier step sans commande
 			this.journal.ajouter("Debut des ventes et premiere production");
@@ -79,7 +79,7 @@ public class Producteur implements Acteur,IProducteur{
 	//pour chaque transformateur a chaque step
 	
 	public double annonceQuantiteMiseEnVente(ITransformateur t) {
-		return (this.prod_tot_utilisable.getValeur()/24);
+		return (this.prodUtilisable.getValeur()/24);
 	}
 	
 	//return un double valant le prix a la tonne du cacao en vente
