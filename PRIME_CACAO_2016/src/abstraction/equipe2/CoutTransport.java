192b4e763facd8c3ca614cd33ae595bc31547a5f
Valentin <Valentin@172.18.144.127>
2016-05-04 11:52:19 +0200

    CoutTransport fini

--- PRIME_CACAO_2016/src/abstraction/equipe2/CoutTransport.java
+++ PRIME_CACAO_2016/src/abstraction/equipe2/CoutTransport.java
@@ -4,15 +4,13 @@ import java.util.HashMap;
 import OldV1transformateur.CommandeProd;
 import abstraction.commun.*;
 import abstraction.equipe1.Producteur;
-import abstraction.fourni.Monde;
+import abstraction.fourni.*;
 
 
 public class CoutTransport {
 	
 	
 	private double couttransport;
-	public static final double DISTANCE_NESTLE_PROD1 = 5000.0;
-	public static final double DISTANCE_NESTLE_PROD2 = 9000.0;
 	private HashMap<IProducteur,Double> distances;
 	
 	
@@ -24,6 +22,11 @@ public class CoutTransport {
 	public void addDistance(IProducteur p, Double dist) {
 		this.distances.put(p,dist);
 	}
+	
+	public HashMap<IProducteur, Double> getDistances() {
+		return distances;
+	}
+	
 	public double getCouttransport() {
 		return couttransport;
 	}
@@ -32,19 +35,16 @@ public class CoutTransport {
 		this.couttransport=couttransport*this.distances.get(p)*commande.getCommandesProd();
 	}
 	
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		double couttransport=0.0015;
-		CommandesProd commande = new CommandesProd(1000);
+		CommandesProd commande = new CommandesProd(1000.0);
 		Producteur p1 = new Producteur(Constantes.NOM_PRODUCTEUR_1, 1000.0, 0.0, Monde.LE_MONDE);
 		Producteur p2 = new Producteur(Constantes.NOM_PRODUCTEUR_2, 1000.0, 0.0, Monde.LE_MONDE);
 		CoutTransport CoutT = new CoutTransport(couttransport);
 		CoutT.addDistance(p1,5000.0);
 		CoutT.addDistance(p2,9000.0);
 		CoutT.setCouttransport(couttransport,p1,commande);
-		if(CoutT()!=couttransport*DISTANCE_NESTLE_PROD1){
-			System.out.println(	CoutT);
-		}
-	}
-	
-
+		System.out.println(CoutT.getCouttransport());
+		System.out.println(CoutT.getDistances());
+		}*/
 }
