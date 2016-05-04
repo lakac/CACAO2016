f17a765ced44140090bb119136eb2a4f0d50e585
Valentin <Valentin@172.18.144.127>
2016-05-04 11:45:50 +0200

    04/05/2016

--- PRIME_CACAO_2016/src/abstraction/equipe2/CoutTransport.java
+++ PRIME_CACAO_2016/src/abstraction/equipe2/CoutTransport.java
@@ -28,17 +28,19 @@ public class CoutTransport {
 		return couttransport;
 	}
 
-	public void setCouttransport(double couttransport, IProducteur p, CommandeProd commande) {
-		this.couttransport=couttransport*this.distances.get(p)*commande.getCommandes();
-		
+	public void setCouttransport(double couttransport, IProducteur p, CommandesProd commande) {
+		this.couttransport=couttransport*this.distances.get(p)*commande.getCommandesProd();
 	}
 	
 	public static void main(String[] args) {
 		double couttransport=0.0015;
+		CommandesProd commande = new CommandesProd(1000);
 		Producteur p1 = new Producteur(Constantes.NOM_PRODUCTEUR_1, 1000.0, 0.0, Monde.LE_MONDE);
 		Producteur p2 = new Producteur(Constantes.NOM_PRODUCTEUR_2, 1000.0, 0.0, Monde.LE_MONDE);
 		CoutTransport CoutT = new CoutTransport(couttransport);
-		CoutT.setCouttransport(couttransport,p1);
+		CoutT.addDistance(p1,5000.0);
+		CoutT.addDistance(p2,9000.0);
+		CoutT.setCouttransport(couttransport,p1,commande);
 		if(CoutT()!=couttransport*DISTANCE_NESTLE_PROD1){
 			System.out.println(	CoutT);
 		}
