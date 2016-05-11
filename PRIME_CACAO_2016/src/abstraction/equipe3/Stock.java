56b0d447f7e62ab3d843258d8973e09050f56a54
Maël <Mal@172.18.159.52>
2016-05-11 11:49:36 +0200

    .

---
+++ PRIME_CACAO_2016/src/abstraction/equipe3/Stock.java
@@ -0,0 +1,31 @@
+package abstraction.equipe3;
+
+import java.util.ArrayList;
+
+import abstraction.commun.ITransformateur;
+
+//public class Stock {
+	
+	//private ArrayList<Double> stock;
+	
+	/*public Stock(){
+		this.stock = new ArrayList<Double>();
+		for (int i=0;i<this.transformateurs.size();i++){
+			this.stock.add(0.0);
+		}
+	}
+	
+	public void setStock(ITransformateur t){
+		double s = 0;
+		double x = 0;
+		for (int i=0;i<this.transformateurs.size();i++){
+			if (t.equals(this.transformateurs.get(i))){
+				x=this.stock.get(i);
+				x += this.getDemande(t)-this.getVente(t);
+				this.stock.remove(i);
+				this.stock.add(i, x);
+				s=this.stock.get(i);
+			}
+		} return s;
+	}
+}*/
