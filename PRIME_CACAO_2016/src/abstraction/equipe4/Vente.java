74fdf473f9a814db36a054b3d7fd6600afa302ae
JVH <JVH@dhcp-3-114.gde.emn.fr>
2016-05-11 12:32:45 +0200

    Vente 11/05 12:32

--- PRIME_CACAO_2016/src/abstraction/equipe4/Vente.java
+++ PRIME_CACAO_2016/src/abstraction/equipe4/Vente.java
@@ -1,16 +1,33 @@
 package abstraction.equipe4;
 
+import abstraction.fourni.Monde;
+
 public class Vente {
+	//vente pour chacun des trois transformateurs
+	private int[] ventes;
+	//stock disponible
 	private Stock stock;
+	//numéro de la step
+	private int step;
+	//on choisit de ne pas dépasser un certain stock, dans la mesure du possible. C'est-à-dire que si notre stock est supérieur à cette valeur, on veut le plus possible.
+	private int maximumStockAutorise;
+	private Producteur producteur;
 	
 	//Constructeurs
 	public Vente () {
-		
+		this.stock = stock;
 	}
 
-	//Getters&Setters
+	//GETTERS AND SETTERS
+	//numéro de la step
+	public int getStep() {
+		return Monde.LE_MONDE.getStep();
+	}
+	public Producteur getProducteur() {
+		return this.producteur;
+	}
 	public Stock getStock() {
-		return stock;
+		return this.getProducteur().getStock();
 	}
 
 	public void setStock() {
@@ -19,7 +36,7 @@ public class Vente {
 
 	//Retourne la moyenne des prix de vente sur les (12?) dernières step
 	public double moyennePrixDeVente () {
-		
+		return 0.0;
 	}
 	
 	
