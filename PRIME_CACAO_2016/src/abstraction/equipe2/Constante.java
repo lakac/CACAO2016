83e6f0fda5f2964cd4260e52d0b31d947340b46b
Valentin <Valentin@172.18.144.127>
2016-05-11 12:45:15 +0200

    constantes

---
+++ PRIME_CACAO_2016/src/abstraction/equipe2/Constante.java
@@ -0,0 +1,19 @@
+package abstraction.equipe2;
+
+public class Constante {
+
+	
+public static final double CHARGES_FIXES=13003370;
+public static final double RATIO_TRANSFORMATION=0.6;
+public static final double TRESORERIE_INITIALE=300000;
+public static final double COUT_DE_TRANSFORMATION=5000;
+
+public static final double PERTE_MINIMALE = 0.2;
+public static final double VARIATION_PERTE =0.1;
+public static final double MARGE_DE_SECURITE = PERTE_MINIMALE + Math.random()*VARIATION_PERTE;
+
+public static final double COUT_UNITAIRE_TRANSPORT = 0.01;
+
+public static final double COUT_STOCK_UNITAIRE = 18.0;
+
+}
