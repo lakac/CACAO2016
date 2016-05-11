efcb258cbb89bbcc12755a500aca3003f7006949
louis <louis.fruleux@etudiant.mines-nantes.fr>
2016-05-11 09:36:21 +0200

    Implementation marche producteur
    reduction du code

--- PRIME_CACAO_2016/src/abstraction/commun/MarcheProducteur.java
+++ PRIME_CACAO_2016/src/abstraction/commun/MarcheProducteur.java
@@ -3,7 +3,6 @@ package abstraction.commun;
 import java.util.ArrayList;
 import abstraction.commun.IProducteur;
 import abstraction.commun.ITransformateur;
-import abstraction.equipe1.Producteur;
 import abstraction.fourni.Acteur;
 import abstraction.fourni.Historique;
 import abstraction.fourni.Indicateur;
@@ -11,15 +10,15 @@ import abstraction.fourni.Monde;
 
 public class MarcheProducteur implements Acteur {
 	//Prix initial avant toute transaction
-	public static double PRIX_DE_BASE = 3000.0;
+	public static final double PRIX_DE_BASE = 3000.0;
 	public static MarcheProducteur LE_MARCHE;
 	//Historique du prix du marché
 	public Indicateur coursCacao;
 	//Variation du coût par step
-	public static double VARIATION_PRIX = 0.02; 
-	//Extremum des prix
-	public static double PRIX_MINIMUM = 2000;
-	public static double PRIX_MAXIMUM = 4000;
+	public static final double VARIATION_PRIX = 0.02; 
+	//Extrema des prix du marche
+	public static final double PRIX_MINIMUM = 2000;
+	public static final double PRIX_MAXIMUM = 4000;
 
 	private ArrayList<IProducteur> producteurs;
 	private ArrayList<ITransformateur> transformateurs;
@@ -30,15 +29,15 @@ public class MarcheProducteur implements Acteur {
 		this.coursCacao = new Indicateur("Cours Cacao",this,PRIX_DE_BASE);
 		Monde.LE_MONDE.ajouterIndicateur(this.coursCacao);
 	}
-	
+
 	public void ajouterTransformateur(ITransformateur transformateur){
 		this.transformateurs.add(transformateur);
 	}
-	
+
 	public void ajouterProducteur(IProducteur producteur){
 		this.producteurs.add(producteur);
 	}
-	
+
 	//Renvoie la valeur actuelle du cout du cacao
 	public double getCours() {
 		return coursCacao.getValeur();
@@ -47,7 +46,7 @@ public class MarcheProducteur implements Acteur {
 	public Historique getHistorique(){
 		return coursCacao.getHistorique();
 	}
-	
+
 	public String getNom() {
 		return "Marche Cacao";
 	}
@@ -58,34 +57,26 @@ public class MarcheProducteur implements Acteur {
 		for (ITransformateur t : this.transformateurs) {
 			for (IProducteur p : this.producteurs){
 				totalQuantitesEnVenteP+=p.annonceQuantiteMiseEnVente(t);
-			}}
+			}
+		}
 		// Toutes les quantites demandees
 		double totalQuantitesDemandeesT = 0.0;
-		for (ITransformateur t0: this.transformateurs) {
-			for (IProducteur p0: this.producteurs){
-				totalQuantitesDemandeesT+=t0.annonceQuantiteDemandee(p0);
-			}}
+		for (ITransformateur t: this.transformateurs) {
+			for (IProducteur p: this.producteurs){
+				totalQuantitesDemandeesT+=t.annonceQuantiteDemandee(p);
+			}
+		}
 		// Si l'offre est superieur a la demande
 		if (totalQuantitesEnVenteP > totalQuantitesDemandeesT){
-			if (coursCacao.getValeur()*(1-VARIATION_PRIX)>PRIX_MINIMUM){
-				coursCacao.setValeur(this, coursCacao.getValeur()*(1-VARIATION_PRIX));
-			}
+			coursCacao.setValeur(this, Math.max(coursCacao.getValeur()*(1-VARIATION_PRIX),PRIX_MINIMUM));
 		}
+		//Si l'offre est inferieur a la demande
+		else if (totalQuantitesEnVenteP < totalQuantitesDemandeesT){
+			coursCacao.setValeur(this,Math.min(coursCacao.getValeur()*(1+VARIATION_PRIX),PRIX_MAXIMUM));
+		}
+		//Si l'offre est exactement la meme que la demande
 		else {
-			//Si l'offre est inferieur a la demande
-			if (totalQuantitesEnVenteP < totalQuantitesDemandeesT){
-				if (coursCacao.getValeur()*(1+VARIATION_PRIX)<PRIX_MAXIMUM){
-					coursCacao.setValeur(this, coursCacao.getValeur()*(1+VARIATION_PRIX));	
-				}
-			}
-			//Si l'offre est exactement la meme que la demande
-			else {
-				coursCacao.setValeur(this, coursCacao.getValeur());
-			}
+			coursCacao.setValeur(this, coursCacao.getValeur()); //On affecte une valeur pour actualiser l'historique
 		}
 	}
-}	
-
-
-
-
+}
