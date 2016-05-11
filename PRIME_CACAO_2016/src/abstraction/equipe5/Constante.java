0ca403aa9db7486c9b2385a99ce8f82a4783001b
May-linh <may.linh@live.fr>
2016-05-11 10:42:21 +0200

    modification constante ratio

--- PRIME_CACAO_2016/src/abstraction/equipe5/Constante.java
+++ PRIME_CACAO_2016/src/abstraction/equipe5/Constante.java
@@ -7,6 +7,6 @@ public class Constante {
 	 public static final int STEP_PRECEDENT = 2;
 	 public static final int STEP_2 = 1;
 	 public static final int STEP_3 = 0;
-	 public static final double[] RATIO_CACAO_CHOCOLAT = {0.7,0.8,0.9}; // pour 100g de chocolat, il faut 60g de cacao
-	 public static final Produit[] listeProduit = {new Produit("70%"),new Produit("80%"),new Produit("90%")};
+	 public static final double[] RATIO_CACAO_CHOCOLAT = {0.6,0.7,0.8}; // pour 100g de chocolat, il faut par exemple 60g de cacao
+	 public static final Produit[] listeProduit = {new Produit("60%"),new Produit("70%"),new Produit("80%")};
 }
