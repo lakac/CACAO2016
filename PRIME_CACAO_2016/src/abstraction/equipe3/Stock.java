a17f142681157b0938419794d7b2b94d0566296d
Maël <Mal@172.18.169.100>
2016-05-13 15:58:56 +0200

    modif class Stock pour avoir 3 produits

--- PRIME_CACAO_2016/src/abstraction/equipe3/Stock.java
+++ PRIME_CACAO_2016/src/abstraction/equipe3/Stock.java
@@ -7,36 +7,38 @@ import abstraction.equipe3.Leclerc;
 
 public class Stock {
 	
-	private ArrayList<Double> stock;
+	private ArrayList<Double[]> stock;
 	private Leclerc leclerc;
 	
-	public Stock(Leclerc leclerc, ArrayList<Double> stock){
+	public Stock(Leclerc leclerc, ArrayList<Double[]> stock){
 		this.leclerc=leclerc;
 		this.stock=stock;
 	}
 	
 	public void initialiseStock(){
-		this.stock = new ArrayList<Double>();
+		this.stock = new ArrayList<Double[]>();
+		Double[] l = {0.0,0.0,0.0};
 		for (int i=0;i<this.leclerc.nombreTransformateur();i++){
-			this.stock.add(0.0);
+			this.stock.add(l);
 		}
 	}
 	
-	public double getStock(ITransformateur t){
+	public double getStock(ITransformateur t, int  indexproduit){
 		double stock=0;
 		for (int i=0;i<this.leclerc.nombreTransformateur();i++){
 			if (t.equals(this.leclerc.getTransformateurs(i))){
-				stock=this.stock.get(i);
+				stock=this.stock.get(i)[indexproduit];
 			}
 		} return stock;
 	}
 	
-	public void setStock (ITransformateur t) {
-		double x = this.getStock(t);
-		x+=this.leclerc.getDemande(t)-this.leclerc.getVente(t);
+	public void setStock (ITransformateur t, int indexproduit) {
+		Double[] x;
 		for (int i=0;i<this.leclerc.nombreTransformateur();i++){
 			if (t.equals(this.leclerc.getTransformateurs(i))){
+				x=this.stock.get(i);
 				this.stock.remove(i);
+				x[indexproduit]+=this.leclerc.getDemande(t)-this.leclerc.getVente(t);
 				this.stock.add(i, x);
 			}
 		} 
