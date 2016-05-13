c3f0d6e6d7797a4ab6d9ef0900f6e83bedc2e449
nessimlegendre <nessimlegendre@dhcp-158-235.invite.emn.fr>
2016-05-13 17:01:49 +0200

    PrixVente2

--- PRIME_CACAO_2016/src/abstraction/equipe6/PrixVente.java
+++ PRIME_CACAO_2016/src/abstraction/equipe6/PrixVente.java
@@ -1,43 +1,51 @@
 package abstraction.equipe6;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import abstraction.fourni.Indicateur;
+import abstraction.commun.CommandeDistri;
+import abstraction.commun.Produit;
 
 public class PrixVente {
 	private List<Double> prixparproduits;
-	private List<Indicateur> historiquedesproduits;
-	private List<String> nomproduits;
+	private List<Produit> nomproduits;
 	private int step;
 	
-	public PrixVente(List<Double> ppp, List<Indicateur> hdp, List<String> nomproduits, int step ){
-		this.historiquedesproduits=hdp;
+	public PrixVente(List<Double> ppp,  List<Produit> nomproduits, int step ){
 		this.prixparproduits=ppp;
 		this.nomproduits=nomproduits;
 		this.step =step;
+	}
+	
+	public PrixVente() {
+		this(Arrays.asList(3.0,3.0,3.0),
+				Arrays.asList(new Produit("p1",5.0),new Produit("p2",5.0),new Produit("p3",1.0)),1);
 	}
 
 	public List<Double> getPrixVente(){
 		return this.prixparproduits;
 	}
-	public List<Indicateur> getHistoriquePrix(){
-		return this.historiquedesproduits;
-	}
 
-}
-	
-	/*public void setPrix ( List<PrixAchat> p1, List<PrixAchat> p2, List<PrixAchat> pautres, PrixVenteLeclerc ) {
-		double prixréf = 1.25*p1+;
-		if (prixréf>1.2*p2) {
+
+	public void setPrix ( List<CommandeDistri> t1, List<CommandeDistri> t2, List<CommandeDistri> tautres, double PrixVenteLeclerc ) {
+	    int c = 0;
+		for (Double d : this.prixparproduits){
+	        d=1.25*(t1.get(c).getQuantite()*t1.get(c).getPrixTonne()+t2.get(c).getQuantite()*t2.get(c).getPrixTonne()+
+	        		tautres.get(c).getQuantite()*tautres.get(c).getPrixTonne())/
+	        		(t1.get(c).getQuantite()+t2.get(c).getQuantite()+tautres.get(c).getQuantite());
+	        c++;
+		/*if (prixréf>1.2*p2) {
 			this.prix = (prixréf - p2);			
 		}
 		else if (prixréf<0.95*p2) {
 			this.prix = prixréf+0.05*p2;
+		}*/
+		
 		}
+	
 	}
 	
-	public void Promotion*/
+}
 	
 	
 	
