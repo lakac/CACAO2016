409632f88054662fbb497ce6b4108e4f3adc378d
Floriane Dahman <floriane.dahman@hotmail.fr>
2016-05-13 15:32:32 +0200

    Préparation du fetch

--- PRIME_CACAO_2016/src/abstraction/equipe5/AchatProd.java
+++ PRIME_CACAO_2016/src/abstraction/equipe5/AchatProd.java
@@ -1,32 +1,39 @@
-package abstraction.equipe5;
-
-import abstraction.commun.Constantes;
-import abstraction.commun.IProducteur;
-import abstraction.commun.MondeV1;
-
-public class AchatProd {
-	private HistoriqueCommandeProduc hist;
-
-	public AchatProd(HistoriqueCommandeProduc hist){
-		this.hist = hist;
-	}
-	
-	
-	// Création d'une fonction qui calcule la quantité demandée en comparant les 2prods
-	
-	
-	/**
-	 * Indique la quantitï¿½ demandï¿½e au producteur p.
-	 */
-	public double annonceQuantiteDemandee(IProducteur p,double annoncePrix, double annonceQuantiteMiseEnVente){ 
-	 // le reste du monde est pris en compte manuellement dans le next
-		double quantiteTotale = Constante.RATIO_CACAO_CHOCOLAT*hist.valeur(Constante.STEP_COURANT);
-		if (p==MondeV1.LE_MONDE.getActeur(Constantes.NOM_PRODUCTEUR_1)) {
-			return (Math.min(0.3*quantiteTotale, annonceQuantiteMiseEnVente));
-		}
-		else {
-			return (Math.min(0.3*quantiteTotale, annonceQuantiteMiseEnVente));
-		}
-		
-	}
-}
+//package abstraction.equipe5;
+//
+//import java.util.List;
+//
+//import abstraction.commun.Constantes;
+//import abstraction.commun.IProducteur;
+//import abstraction.commun.MondeV1;
+//
+//public class AchatProd {
+//	private HistoriqueCommandeProduc hist;
+//
+//	public AchatProd(HistoriqueCommandeProduc hist){
+//		this.hist = hist;
+//	}
+//	
+//	
+//	// Création d'une fonction qui calcule la quantité demandée en comparant les 2prods
+//	public double calculQuantiteDemandee(List listeCommandesDist){
+//		
+//		if (annonceMiseEnVente<= )
+//		
+//		return 0.0;
+//	}
+//	
+//	/**
+//	 * Indique la quantitï¿½ demandï¿½e au producteur p.
+//	 */
+//	public double annonceQuantiteDemandee(IProducteur p,double annoncePrix, double annonceQuantiteMiseEnVente){ 
+//	 // le reste du monde est pris en compte manuellement dans le next
+//		double quantiteTotale = Constante.RATIO_CACAO_CHOCOLAT*hist.valeur(Constante.STEP_COURANT);
+//		if (p==MondeV1.LE_MONDE.getActeur(Constantes.NOM_PRODUCTEUR_1)) {
+//			return (Math.min(0.3*quantiteTotale, annonceQuantiteMiseEnVente));
+//		}
+//		else {
+//			return (Math.min(0.3*quantiteTotale, annonceQuantiteMiseEnVente));
+//		}
+//		
+//	}
+//}
