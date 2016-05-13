183597619a2ea9ef4c212a99a5fe4654487b7e70
Floriane Dahman <floriane.dahman@hotmail.fr>
2016-05-13 16:40:02 +0200

    AchatProd

--- PRIME_CACAO_2016/src/abstraction/equipe5/AchatProd.java
+++ PRIME_CACAO_2016/src/abstraction/equipe5/AchatProd.java
@@ -9,39 +9,41 @@ import abstraction.commun.MondeV1;
 
 public class AchatProd {
 	private HistoriqueCommandeProduc hist;
+	private Lindt lindt;
 
-	public AchatProd(HistoriqueCommandeProduc hist){
+	public AchatProd(HistoriqueCommandeProduc hist, Lindt lindt){
 		this.hist = hist;
+		this.lindt = lindt;
 	}
 	
 	
-//	// Création d'une fonction qui calcule la quantité demandée en comparant les 2prods
-//	public double calculQuantiteDemandee(List<CommandeDistri> listeCommandesDist){
-//		double quantiteTotale=0;
-//		for (CommandeDistri c : listeCommandesDist){
-//			quantiteTotale += c.getQuantite();
-//		}
-//		double quantiteEnVente = 0;
-//		for (IProducteur p: getProducteurs()){
-//			quantiteEnVente += p.annonceQuantiteMiseEnVente(Lindt);
-//		}
-//		if ( <= quantiteTotale)
-//		
-//		return 0.0;
-//	}
+	// Création d'une fonction qui calcule la quantité demandée en comparant les 2prods
+	public double calculQuantiteDemandee(List<CommandeDistri> listeCommandesDist){
+		double quantiteTotale=0;
+		for (CommandeDistri c : listeCommandesDist){
+			quantiteTotale += c.getQuantite();
+		}
+		double quantiteEnVente = 0;
+		for (IProducteur p: lindt.getProducteurs()){
+			quantiteEnVente += p.annonceQuantiteMiseEnVente(lindt);
+		}
+		if (quantiteTotale <= quantiteTotale)
+		
+		return 0.0;
+	}
 	
-//	/**
-//	 * Indique la quantite demandee au producteur p.
-//	 */
-//	public double annonceQuantiteDemandee(IProducteur p,double annoncePrix, double annonceQuantiteMiseEnVente){ 
-//	 // le reste du monde est pris en compte manuellement dans le next
-//		double quantiteTotale = Constante.RATIO_CACAO_CHOCOLAT*hist.valeur(Constante.STEP_COURANT);
-//		if (p==MondeV1.LE_MONDE.getActeur(Constantes.NOM_PRODUCTEUR_1)) {
-//			return (Math.min(0.3*quantiteTotale, annonceQuantiteMiseEnVente));
-//		}
-//		else {
-//			return (Math.min(0.3*quantiteTotale, annonceQuantiteMiseEnVente));
-//		}
-//		
-//	}
+	/**
+	 * Indique la quantite demandee au producteur p.
+	 */
+	public double annonceQuantiteDemandee(IProducteur p,double annoncePrix, double annonceQuantiteMiseEnVente){ 
+	 // le reste du monde est pris en compte manuellement dans le next
+		double quantiteTotale = Constante.RATIO_CACAO_CHOCOLAT*hist.valeur(Constante.STEP_COURANT);
+		if (p==MondeV1.LE_MONDE.getActeur(Constantes.NOM_PRODUCTEUR_1)) {
+			return (Math.min(0.3*quantiteTotale, annonceQuantiteMiseEnVente));
+		}
+		else {
+			return (Math.min(0.3*quantiteTotale, annonceQuantiteMiseEnVente));
+		}
+		
+	}
 }
