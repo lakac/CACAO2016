518f5782fb317031b489434156f30c59c33b184a
nessimlegendre <nessimlegendre@dhcp-158-235.invite.emn.fr>
2016-05-13 14:45:51 +0200

    PrixVente

--- PRIME_CACAO_2016/src/abstraction/equipe6/PrixVente.java
+++ PRIME_CACAO_2016/src/abstraction/equipe6/PrixVente.java
@@ -1,24 +1,45 @@
 package abstraction.equipe6;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import abstraction.fourni.Indicateur;
+
 public class PrixVente {
-	
-	private double prix;
-	private String produit;
+	private List<Double> prixparproduits;
+	private List<Indicateur> historiquedesproduits;
+	private List<String> nomproduits;
 	private int step;
 	
-	public PrixVente(double prix, String produit, int step) {
-		this.prix=prix;
-		this.produit=produit;
-		this.step=step;
+	public PrixVente(List<Double> ppp, List<Indicateur> hdp, List<String> nomproduits, int step ){
+		this.historiquedesproduits=hdp;
+		this.prixparproduits=ppp;
+		this.nomproduits=nomproduits;
+		this.step =step;
+	}
+
+	public List<Double> getPrixVente(){
+		return this.prixparproduits;
+	}
+	public List<Indicateur> getHistoriquePrix(){
+		return this.historiquedesproduits;
+	}
+
+}
+	
+	/*public void setPrix ( List<PrixAchat> p1, List<PrixAchat> p2, List<PrixAchat> pautres, PrixVenteLeclerc ) {
+		double prixréf = 1.25*p1+;
+		if (prixréf>1.2*p2) {
+			this.prix = (prixréf - p2);			
+		}
+		else if (prixréf<0.95*p2) {
+			this.prix = prixréf+0.05*p2;
+		}
 	}
 	
-	public void setPrix ( PrixAchat p) {
-		this.prix = 1.25*p;
-	}
-	
-	public void Promotion
+	public void Promotion*/
 	
 	
 	
 
-}
+
