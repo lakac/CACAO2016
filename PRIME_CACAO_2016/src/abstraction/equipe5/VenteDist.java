367acff78c0daeb65980d6885951d84e90141616
May-linh <may.linh@live.fr>
2016-05-20 14:11:11 +0200

    test sauvegarde

---
+++ PRIME_CACAO_2016/src/abstraction/equipe5/VenteDist.java
@@ -0,0 +1,21 @@
+package abstraction.equipe5;
+import abstraction.equipe5.Lindt;
+
+import java.util.List;
+
+import abstraction.commun.CommandeDistri;
+
+
+public class VenteDist {
+	
+	private String produit;
+	private Lindt lindt;
+	
+	public VenteDist(String p, Lindt lindt){
+		this.produit=p;
+		this.lindt=lindt;
+	}
+
+	
+
+}
