1359870dcdb74cbedcfdd2288da36d00f6a22c1f
jhtr <jhtr@users.noreply.github.com>
2016-05-21 18:25:53 +0200

    Utilisation de fonctions au lieu de variables

--- PRIME_CACAO_2016/src/abstraction/equipe1/IntelligenceEconomique.java
+++ PRIME_CACAO_2016/src/abstraction/equipe1/IntelligenceEconomique.java
@@ -9,10 +9,7 @@ import abstraction.commun.MarcheProducteur;
 public class IntelligenceEconomique {
 	private List<ITransformateur> transformateurs;
 	private HashMap<ITransformateur,Double> importanceTransformateurs;
-	private double envieVendre;
-	private double besoinVendre;
 	private Stock stock;
-	private double offreTotale;
 	private HashMap<ITransformateur,Double> quantitesMisesEnVente;
 	
 	private final double[] coeffPerissabilite = {1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1};
@@ -20,10 +17,7 @@ public class IntelligenceEconomique {
 	public IntelligenceEconomique(List<ITransformateur> t, Stock s) {
 		this.transformateurs = t;
 		this.importanceTransformateurs = new HashMap<ITransformateur,Double>();
-		this.envieVendre = 0;
-		this.besoinVendre = 0;
 		this.stock = s;
-		this.offreTotale = 0;
 		this.quantitesMisesEnVente = new HashMap<ITransformateur,Double>();
 		
 		for (ITransformateur tr : this.transformateurs) {
@@ -32,19 +26,23 @@ public class IntelligenceEconomique {
 		}
 	}
 	
-	private void actualiserEnvieVendre() {
-		this.envieVendre = Math.sin((MarcheProducteur.LE_MARCHE.getCours()-2000.0)/2000.0);
+	private double calculerEnvieDeVendre() {
+		double rapport = (MarcheProducteur.LE_MARCHE.getCours()-MarcheProducteur.PRIX_MINIMUM)/(MarcheProducteur.PRIX_MAXIMUM-MarcheProducteur.PRIX_MINIMUM);
+		return (1.0 + Math.sin((rapport - 0.5) * Math.PI)) / 2.0;
 	}
 	
-	private void actualiserBesoinVendre() {
-		this.besoinVendre = 0;
+	private double calculerBesoinDeVendre() {
+		double besoin = 0.0;
 		for (int i = 0; i<this.coeffPerissabilite.length; i++) {
-			this.besoinVendre += this.coeffPerissabilite[i]*this.stock.getStockParStep(i);
+			besoin += this.coeffPerissabilite[i]*this.stock.getStockParStep(i);
 		}
+		return besoin;
 	}
 	
-	private void actualiserOffreTotale() {
-		this.offreTotale = this.besoinVendre*(1-this.envieVendre) + this.stock.getQuantite()*this.envieVendre;
+	private double calculerOffreTotale() {
+		double envie = calculerEnvieDeVendre();
+		double besoin = calculerBesoinDeVendre();
+		return besoin*(1.0-envie) + this.stock.getQuantite()*envie;
 	}
 	
 	private void actualiserImportanceTransformateurs() {
@@ -53,30 +51,26 @@ public class IntelligenceEconomique {
 			beneficeTotal += t.annoncePrix()*t.annonceQuantiteDemandee();
 		}
 		
-		double valeur = 0;
 		for (ITransformateur t : this.transformateurs) {
-			valeur = this.importanceTransformateurs.get(t)*0.8 + t.annoncePrix()*t.annonceQuantiteDemandee()*0.2/beneficeTotal;
+			double valeur = this.importanceTransformateurs.get(t)*0.8 + t.annoncePrix()*t.annonceQuantiteDemandee()*0.2/beneficeTotal;
 			this.importanceTransformateurs.put(t, valeur);
 		}
 	}
 	
 	private void actualiserQuantitesMisesEnVente() {
-		double importance = 0;
+		double importance = 0.0;
 		for (ITransformateur t : this.transformateurs) {
 			importance += this.importanceTransformateurs.get(t);
 		}
 		
-		double valeur = 0;
+		double offreTotale = calculerOffreTotale();
 		for (ITransformateur t : this.transformateurs) {
-			valeur = this.offreTotale*this.importanceTransformateurs.get(t)/importance;
+			double valeur = offreTotale*this.importanceTransformateurs.get(t)/importance;
 			this.quantitesMisesEnVente.put(t, valeur);
 		}
 	}
 	
 	public void actualiser() {
-		this.actualiserEnvieVendre();
-		this.actualiserBesoinVendre();
-		this.actualiserOffreTotale();
 		this.actualiserImportanceTransformateurs();
 		this.actualiserQuantitesMisesEnVente();
 	}
