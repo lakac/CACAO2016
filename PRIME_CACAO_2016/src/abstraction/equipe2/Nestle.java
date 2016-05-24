3e2dc66544bb408503d8e82188d2cc26f347597d
Alexandre MARTY <alexandre.marty@ymail.com>
2016-05-24 10:26:48 +0200

    24/05

--- PRIME_CACAO_2016/src/abstraction/equipe2/Nestle.java
+++ PRIME_CACAO_2016/src/abstraction/equipe2/Nestle.java
@@ -151,7 +151,7 @@ public class Nestle implements Acteur, ITransformateur{
 
 	@Override
 	public List<CommandeDistri> CommandeFinale(List<CommandeDistri> cf) {
-		return null;
+		return Offre(cf);
 	}
 
 	@Override
