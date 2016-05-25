3c663704d4be5fc39d455bd0d943ee0992939dc7
JMagloire <JMag@dhcp-4-89.invite.emn.fr>
2016-05-25 21:09:47 +0200

    modif ini Lindt

--- PRIME_CACAO_2016/src/abstraction/equipe5/Lindt.java
+++ PRIME_CACAO_2016/src/abstraction/equipe5/Lindt.java
@@ -27,9 +27,8 @@ public class Lindt implements Acteur, ITransformateur{
 	public Lindt(){
 		this.histCommandeDistri = new HistoriqueCommandeDistri();
 		this.histCommandeProduc = new HistoriqueCommandeProduc();
-		for (IProducteur p : this.getProducteurs()) {
-			this.histCommandeProduc.ajouter(new CommandeProduc(this, p, 100.0, MarcheProducteur.LE_MARCHE.getCours()));
-		}
+		this.histCommandeProduc.ajouter(new CommandeProduc(this, this.getProducteurs().get(0), 100.0, MarcheProducteur.LE_MARCHE.getCours()));
+		this.histCommandeProduc.ajouter(new CommandeProduc(this, this.getProducteurs().get(1), 100.0, MarcheProducteur.LE_MARCHE.getCours()));
 		this.stockCacao = new Stock("cacao",this,0.0);
 		this.stockChocolat50 = new Stock(Constante.LISTE_PRODUIT[0].getNomProduit(),this,0.0);
 		this.stockChocolat60 = new Stock(Constante.LISTE_PRODUIT[1].getNomProduit(),this,0.0);
