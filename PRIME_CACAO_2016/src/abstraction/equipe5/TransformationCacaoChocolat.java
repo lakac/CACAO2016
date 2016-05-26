34757324faa80e6921f74bdf534e0514d5409f23
May-linh <may.linh@live.fr>
2016-05-26 19:29:14 +0200

    creation de transformation

---
+++ PRIME_CACAO_2016/src/abstraction/equipe5/TransformationCacaoChocolat.java
@@ -0,0 +1,26 @@
+package abstraction.equipe5;
+
+import abstraction.commun.CommandeDistri;
+import abstraction.fourni.Monde;
+
+public class TransformationCacaoChocolat {
+	private Lindt lindt;
+
+	public TransformationCacaoChocolat(Lindt lindt) {
+		this.lindt = lindt;
+	}
+	
+	public void Transformation(){
+		for (CommandeDistri c : lindt.getHistCommandeDistri().getHist()){
+			if (c.getStepLivraison()==Monde.LE_MONDE.getStep()+2){
+				lindt.getStockCacao().setStock((lindt.getStockCacao().getStock()-c.getQuantite()*c.getProduit().getRatioCacao()));
+				for (int i=0; i<lindt.getStocksChocolat().size(); i++){
+					if (c.getProduit().getNomProduit()==lindt.getStocksChocolat().get(i).getNom()){
+						lindt.getStocksChocolat().get(i).setStock(lindt.getStocksChocolat().get(i));// a finir
+					}
+			}
+		}
+	}
+	
+
+}
