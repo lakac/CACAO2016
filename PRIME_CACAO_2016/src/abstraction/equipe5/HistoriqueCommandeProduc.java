3bcc3a4caa1f85140bb3aea5823604273727fb7e
JMagloire <JMag@dhcp-4-89.invite.emn.fr>
2016-05-26 18:23:26 +0200

    correction Historique Produc

--- PRIME_CACAO_2016/src/abstraction/equipe5/HistoriqueCommandeProduc.java
+++ PRIME_CACAO_2016/src/abstraction/equipe5/HistoriqueCommandeProduc.java
@@ -4,18 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import abstraction.commun.CommandeProduc;
 import abstraction.commun.IProducteur;
-import abstraction.commun.MarcheProducteur;
 
 public class HistoriqueCommandeProduc {
 	private List<CommandeProduc> hist = new ArrayList<CommandeProduc>();
 	private ArrayList<IProducteur> producteurs;
-	private Lindt lindt;
 
 	public HistoriqueCommandeProduc() {	
 		this.hist = new ArrayList<CommandeProduc>();
-		// voir avec le prof comment initialiser des commandes
-//		this.hist.add(new CommandeProduc(lindt, this.getProducteurs().get(0), 100.0, MarcheProducteur.LE_MARCHE.getCours()));
-//		this.hist.add(new CommandeProduc(lindt, this.getProducteurs().get(1), 100.0, MarcheProducteur.LE_MARCHE.getCours()));
 	}
 
 	public List<CommandeProduc> getHist() {
