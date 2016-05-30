b458c3a23239ee79a66fdc7deaa2a219d41fc117
JMagloire <JMag@dhcp-4-89.invite.emn.fr>
2016-05-30 16:57:02 +0200

    J'ai essayé de résoudre le pb du code faux dans Lindt, mais il y a une erreur dans le code du MarcheDistributeur (ou plutôt il manque qqc), ce qui fait qu'on ne peut pas appeler cette méthode …

--- PRIME_CACAO_2016/src/abstraction/equipe5/Lindt.java
+++ PRIME_CACAO_2016/src/abstraction/equipe5/Lindt.java
@@ -112,7 +112,9 @@ public class Lindt implements Acteur, ITransformateur{
 		this.getTransformationCacaoChocolat().Transformation(); // transforme le cacao en chocolat et met à jour les stocks (retire pour cacao et ajoute pour chocolat)
 		
 		for(IDistributeur d: this.getDistributeurs()){ // ajout des commandes finales à notre historique
-			for (CommandeDistri cd : LE_MARCHE_DISTRIBUTEUR.obtenirCommandeFinale(this,d)){
+			for (CommandeDistri cd : MarcheDistributeur.LE_MARCHE_DISTRIBUTEUR.obtenirCommandeFinale(this,d)){ 
+				//tant que les distributeurs ne créent pas dans le marche une variable d'instance static 
+				// (public static MarcheDistributeur LE_MARCHE_DISTRIBUTEUR;), on ne pourra pas appeler cette méthode
 				this.getHistCommandeDistri().ajouter(cd);
 		}}
 		
