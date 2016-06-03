a6e9a9fa72b85b0592b3a83a90d46a7a83fa37d9
Valentin <Valentin@172.18.226.63>
2016-06-03 14:00:55 +0200

    pour sabrina

--- PRIME_CACAO_2016/src/abstraction/commun/MondeV1.java
+++ PRIME_CACAO_2016/src/abstraction/commun/MondeV1.java
@@ -71,13 +71,7 @@ public class MondeV1 extends Monde {
 		// Ajout des acteurs dans les listes des acteurs
 
 		
-		lindt.ajouterProducteur(p1);
-		lindt.ajouterProducteur(p2);
-
-		t3.ajouterTransformateur(nestle);
-
-		t3.ajouterTransformateur(lindt);
-		// Ajout des liens necessaires entre les acteurs
+		
 		Le.ajouterVendeur(nestle);
 		Le.ajouterVendeur(lindt);
 		
