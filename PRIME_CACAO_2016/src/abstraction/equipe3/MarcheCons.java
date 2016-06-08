1e41e9d8446458701575d6e425d301e7a1f9be14
Felisque <POIRIER.FELIX49@GMAIL.COM>
2016-06-08 11:05:38 +0200

    Commit

--- PRIME_CACAO_2016/src/abstraction/equipe3/MarcheCons.java
+++ PRIME_CACAO_2016/src/abstraction/equipe3/MarcheCons.java
@@ -36,13 +36,29 @@ public class MarcheCons {
 	
 	private List<CommandeDistri> ventesEffectuees;
 	
-	private HashMap <Produit,Double> demandeAnnuelle; // volume des ventes annuelles d'un produit
+	/*volume des ventes annuelles d'un produit*/
+	
+	private HashMap <Produit,Double> demandeAnnuelle; 
+	
+	/*composante continue de la demande par step*/
+	
 	private HashMap <Produit,Double> demandeComposanteContinue;
+	
+	/*composante aleatoire de la demande par step*/
+	
 	private HashMap <Produit,Double> demandeComposanteAleatoire;
-	private HashMap <Integer, HashMap<Produit,Double>> calendrierDemande; 
+	
+	/*calendrier comportant la demande à chaque step*/
+	
+	private List<Demande> calendrierDemande; 
+	
+	/*variable qui regle l'amplitude de la composante aleatoire*/
+	
 	private HashMap <Produit,Double> pourcentageIncertitudeVentes;
-	private HashMap <Produit,Double> offreTotale;
-	private HashMap <IDistributeur,HashMap<Produit,Double>> fidelite ;
+	
+	/*variable qui stocke la part de marche de chaque produit pour chaque distributeur*/
+	
+	private List<Fidelite> fidelite ;
 	
 
 	public MarcheCons(String nom, ArrayList<Produit> produits) {
@@ -51,13 +67,12 @@ public class MarcheCons {
 		MarcheCons.distributeurs=new ArrayList<IDistributeur>();
 		MarcheCons.transformateurs=new ArrayList<ITransformateur>();
 		this.ventesEffectuees=new ArrayList<CommandeDistri>();
-		this.calendrierDemande=new HashMap <Integer, HashMap<Produit,Double>>();
+		this.calendrierDemande=new ArrayList<Demande>();
 		this.demandeAnnuelle=new HashMap <Produit,Double>();
 		this.demandeComposanteContinue=new HashMap <Produit,Double>();
 		this.demandeComposanteAleatoire=new HashMap <Produit,Double>();
 		this.pourcentageIncertitudeVentes=new HashMap <Produit,Double>();
-		this.offreTotale=new HashMap <Produit,Double>();
-		this.fidelite = new HashMap <IDistributeur,HashMap<Produit,Double>>();
+		this.fidelite = new ArrayList<Fidelite>();
 		// TODO Auto-generated constructor stub
 	}
 	
@@ -92,15 +107,15 @@ public class MarcheCons {
 		for (Produit p : this.getProduits()){
 			for (int i=1;i<=26;i++){
 				if (i%26==6){
-					this.calendrierDemande.get(i).put(p, 0.0735*this.demandeAnnuelle.get(p));
+				//	this.calendrierDemande.get(i).put(p, 0.0735*this.demandeAnnuelle.get(p));
 					
 				}
 				if (i%26==25){
-					this.calendrierDemande.get(i).put(p, 0.1235*this.demandeAnnuelle.get(p));
+				//	this.calendrierDemande.get(i).put(p, 0.1235*this.demandeAnnuelle.get(p));
 					
 				}
 				else{
-					this.calendrierDemande.get(i).put(p, 0.0335*this.demandeAnnuelle.get(p));	
+				//	this.calendrierDemande.get(i).put(p, 0.0335*this.demandeAnnuelle.get(p));	
 				}
 			}
 		}
@@ -121,11 +136,11 @@ public class MarcheCons {
 	
 	public void initialiserFidelite(){
 		for (Produit p : this.getProduits()){
-			this.fidelite.get(MarcheCons.distributeurs.get(0)).put(p, 0.3);
-			this.fidelite.get(MarcheCons.distributeurs.get(1)).put(p, 0.3);
+			//this.fidelite.get(MarcheCons.distributeurs.get(0)).put(p, 0.3);
+			//this.fidelite.get(MarcheCons.distributeurs.get(1)).put(p, 0.3);
 			for (IDistributeur d : MarcheCons.distributeurs){
 				if(!(d.equals(MarcheCons.distributeurs.get(1))&&d.equals(MarcheCons.distributeurs.get(0)))){
-					this.fidelite.get(d).put(p, 0.4); //0.4 n'est valable que si 3 distributeurs dans le monde
+				//	this.fidelite.get(d).put(p, 0.4); //0.4 n'est valable que si 3 distributeurs dans le monde
 				}
 				
 			}
