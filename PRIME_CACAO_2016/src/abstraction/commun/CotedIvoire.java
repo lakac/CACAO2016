86941afee63ee534341ff03412199f72dff7b38d
Alexandre MARTY <alexandre.marty@ymail.com>
2016-06-08 08:49:19 +0200

    8/06

---
+++ PRIME_CACAO_2016/src/abstraction/commun/CotedIvoire.java
@@ -0,0 +1,22 @@
+package abstraction.commun;
+
+//Ajout conformément à la réunion du vendredi 3/06 par l'équipe 2 d'un troisième producteur mondial
+
+public class CotedIvoire implements IProducteur {
+
+	@Override
+	public double annonceQuantiteMiseEnVente(ITransformateur t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	
+	@Override
+	public void notificationVente(CommandeProduc c) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
