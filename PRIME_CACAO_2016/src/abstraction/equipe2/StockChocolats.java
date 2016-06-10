1f6672406063470e4582d3fc4f2c1c834df0e7d0
Valentin <Valentin@Valentin-PC>
2016-06-10 17:19:29 +0200

    stocks finaux

--- PRIME_CACAO_2016/src/abstraction/equipe2/StockChocolats.java
+++ PRIME_CACAO_2016/src/abstraction/equipe2/StockChocolats.java
@@ -5,29 +5,26 @@ import java.util.HashMap;
 import abstraction.commun.Produit;
 
 public class StockChocolats extends Stock {
-
-	
-private HashMap<Produit,Double> stockChocolats;
 	
 	public StockChocolats(){
 		super();
-			this.stockChocolats.put(Constante.PRODUIT_50, 0.);
-			this.stockChocolats.put(Constante.PRODUIT_60, 0.);
-			this.stockChocolats.put(Constante.PRODUIT_70, 0.);
+			super.getStock().put(Constante.PRODUIT_50, 0.);
+			super.getStock().put(Constante.PRODUIT_60, 0.);
+			super.getStock().put(Constante.PRODUIT_70, 0.);
 	}
 	
 	public HashMap<Produit, Double> getStockchocolats() {
-		return stockChocolats;
+		return super.getStock();
 	}
 
 	@Override
 	public void MiseAJourStockLivraison(Produit p,double quantite) {
-		this.stockChocolats.put(p, this.stockChocolats.get(p)-quantite);
+		this.getStockchocolats().put(p, this.getStockchocolats().get(p)-quantite);
 	}
 
 	@Override
 	public void MiseAJourStockTransformation(Produit p, double quantite) {
-		this.stockChocolats.put(p, this.stockChocolats.get(p)+quantite);		
+		this.getStockchocolats().put(p, this.getStockchocolats().get(p)+quantite);		
 	}
 
 
