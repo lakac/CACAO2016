a596af533df3843d716adb8406f25a3795282c0f
Valentin <Valentin@Valentin-PC>
2016-06-11 01:09:22 +0200

    stocks test

--- PRIME_CACAO_2016/src/abstraction/equipe2/StockCacao.java
+++ PRIME_CACAO_2016/src/abstraction/equipe2/StockCacao.java
@@ -11,6 +11,9 @@ public class StockCacao extends Stock {
 	public StockCacao(){
 		super();
 			super.getStock().put(Constante.CACAO, 0.);
+			super.getStock().put(Constante.PRODUIT_50, 0.);
+			super.getStock().put(Constante.PRODUIT_60, 0.);
+			super.getStock().put(Constante.PRODUIT_70, 0.);
 	}
 	
 	
@@ -29,14 +32,70 @@ public class StockCacao extends Stock {
 	// Dans MiseAJOurStockTransformation, p est un chocolat et quantite et une quantite de chocolat voulue
 	public void MiseAJourStockTransformation(Produit p, double quantite) {
 		if (p.equals(Constante.PRODUIT_50)) {
-			this.getStockcacao().put(p,Constante.RATIO_TRANSFORMATION_50*quantite);
+			if(this.getStockcacao().get(Constante.CACAO)-Constante.RATIO_TRANSFORMATION_50*quantite<0){
+				this.getStockcacao().put(Constante.CACAO, 0.);
+			}else{
+				this.getStockcacao().put(Constante.CACAO, this.getStockcacao().get(Constante.CACAO)-Constante.RATIO_TRANSFORMATION_50*quantite);
+			}
 		}
 		else if (p.equals(Constante.PRODUIT_60)) {
-			this.getStockcacao().put(p,Constante.RATIO_TRANSFORMATION_60*quantite);
+			if(this.getStockcacao().get(Constante.CACAO)-Constante.RATIO_TRANSFORMATION_60*quantite<0){
+				this.getStockcacao().put(Constante.CACAO, 0.);
+			}else{
+				this.getStockcacao().put(Constante.CACAO, this.getStockcacao().get(Constante.CACAO)-Constante.RATIO_TRANSFORMATION_60*quantite);
+			}
 		}
 		else if (p.equals(Constante.PRODUIT_70)) {
-			this.getStockcacao().put(p,this.getStockcacao().get(p)-Constante.RATIO_TRANSFORMATION_70*quantite);
-		};
+			if(this.getStockcacao().get(Constante.CACAO)-Constante.RATIO_TRANSFORMATION_70*quantite<0){
+				this.getStockcacao().put(Constante.CACAO, 0.);
+			}else{
+				this.getStockcacao().put(Constante.CACAO, this.getStockcacao().get(Constante.CACAO)-Constante.RATIO_TRANSFORMATION_70*quantite);
+			}
+		}
+		
 	}
+	
+	public static void main(String[] args) {
+		StockCacao stockcac = new StockCacao();
+		//Test constructeur / getter / Initialisation
+		if(stockcac.getStockcacao().get(Constante.CACAO)==0.){
+			System.out.println("Le constructeur et les getter sont bons");
+		}else{
+			System.out.println("Problème inconnu");
+		}
+		//Test de la methode MiseAJourStockLivraison
+		stockcac.MiseAJourStockLivraison(Constante.CACAO, 50);
+		if(stockcac.getStockcacao().get(Constante.CACAO)==50.){
+			System.out.println("La méthode MiseAJourStockLivraison est bonne");
+		}else{
+			System.out.println("Problème inconnu");
+		}
+		//Test de la methode MiseAJourStockTransformation
+		stockcac.MiseAJourStockTransformation(Constante.PRODUIT_50, 25.);
+		if(stockcac.getStockcacao().get(Constante.CACAO)==37.5){
+			System.out.println("La methode MiseAJourStockTransformation est bonne");
+		}else{
+			if(stockcac.getStockcacao().get(Constante.CACAO)==50.){
+				System.out.println("La methode MiseAJourStockTransformation ne fait rien");
+			}else{
+				System.out.println("Problème inconnu");
+			}
+		}
+		stockcac.MiseAJourStockTransformation(Constante.PRODUIT_50, 100.);
+		if(stockcac.getStockcacao().get(Constante.CACAO)==0.){
+			System.out.println("La methode MiseAJourStockTransformation est bonne, le stock ne peut pas être négatif");
+		}else{
+			if(stockcac.getStockcacao().get(Constante.CACAO)==37.5){
+				System.out.println("La methode ne fait rien du tout");
+			}else{
+				if(stockcac.getStockcacao().get(Constante.CACAO)==-12.5){
+					System.out.println("Le stock peut être négatif");
+				}else{
+					System.out.println("Problème de stock");
+				}
+			}
+		}
+	}
+		
 
 }
--- PRIME_CACAO_2016/src/abstraction/equipe2/StockChocolats.java
+++ PRIME_CACAO_2016/src/abstraction/equipe2/StockChocolats.java
@@ -17,14 +17,58 @@ public class StockChocolats extends Stock {
 		return super.getStock();
 	}
 
+	//Il faut encore rajouter le fait qu'on ne peut pas livrer plus que ce que l'on a
 	@Override
 	public void MiseAJourStockLivraison(Produit p,double quantite) {
-		this.getStockchocolats().put(p, this.getStockchocolats().get(p)-quantite);
+		if(this.getStockchocolats().get(Constante.PRODUIT_50)-quantite<0){
+			this.getStockchocolats().put(p, 0.);
+			System.out.println("Il n'y a pas assez de chocolat pour tout livrer");
+		}else{
+			this.getStockchocolats().put(p, this.getStockchocolats().get(p)-quantite);
+		}
 	}
 
+	
 	@Override
 	public void MiseAJourStockTransformation(Produit p, double quantite) {
 		this.getStockchocolats().put(p, this.getStockchocolats().get(p)+quantite);		
+	}
+	
+	
+	public static void main(String[] args) {
+		StockChocolats stockchoc = new StockChocolats();
+		//Test Constructeur / Initialisation à 0 / methode getStockchocolats
+		System.out.println(stockchoc.getStockchocolats().get(Constante.PRODUIT_50));
+		System.out.println(stockchoc.getStockchocolats().get(Constante.PRODUIT_60));
+		System.out.println(stockchoc.getStockchocolats().get(Constante.PRODUIT_70));
+		//Test methode MiseAJourStockTransformation
+		stockchoc.MiseAJourStockTransformation(Constante.PRODUIT_50, 100);
+		if (stockchoc.getStockchocolats().get(Constante.PRODUIT_50)==100){
+			System.out.println("La methode MiseAJourStockTransformation fonctionne");
+		}else{
+			System.out.println("La méthode MiseAJourStockTransformation ne fonctionne pas");
+		}
+		
+		//Test methode MiseAJourStockLivraison
+		stockchoc.MiseAJourStockLivraison(Constante.PRODUIT_50, 50);
+		if(stockchoc.getStockchocolats().get(Constante.PRODUIT_50)==50){
+			System.out.println("La methode MiseAJourLivraison bonne");
+		}else{
+			System.out.println("La méthode MiseAJourLivraison ne fonctionne pas");
+		}
+		
+		stockchoc.MiseAJourStockLivraison(Constante.PRODUIT_50, 100);
+		if(stockchoc.getStockchocolats().get(Constante.PRODUIT_50)==0){
+			System.out.println("Test validé : Le stock ne peut pas être négatif");
+		}else{
+			if(stockchoc.getStockchocolats().get(Constante.PRODUIT_50)<0){
+				System.out.println("Erreur dans la méthode : Le stock peut être négatif");
+			}else{
+				System.out.println("Je ne sais pas d'où vient l'erreur");
+			}
+		}
+
+	
 	}
 
 
