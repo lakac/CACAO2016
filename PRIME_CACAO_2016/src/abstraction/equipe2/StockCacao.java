94f596259f10366fc812a52a908fdf1b604af4c4
Sabrina <Sabrina@172.18.252.197>
2016-06-10 16:16:05 +0200

    Nouvelles classes

--- PRIME_CACAO_2016/src/abstraction/equipe2/StockCacao.java
+++ PRIME_CACAO_2016/src/abstraction/equipe2/StockCacao.java
@@ -1,35 +1,17 @@
 package abstraction.equipe2;
 
-import abstraction.commun.Produit;
-
 public class StockCacao extends Stock {
-	private double stockcacao;
 
-	public double getStockcacao() {
-		return stockcacao;
-	} 
-	
-	public void AjouterStockCacao(Achat achat) {
-		this.stockcacao += achat.getCacaoachete();
-	}
-	
-	public void RetirerStockCacao(Produit p, Production production) {
-		if (p.equals(Constante.PRODUIT_50)) {
-			this.stockcacao -= Constante.RATIO_TRANSFORMATION_50*production.getProduction().get(p);
-		}
-		else if (p.equals(Constante.PRODUIT_60)) {
-			this.stockcacao -= Constante.RATIO_TRANSFORMATION_60*production.getProduction().get(p);
-		}
-		else if (p.equals(Constante.PRODUIT_70)) {
-			this.stockcacao -= Constante.RATIO_TRANSFORMATION_70*production.getProduction().get(p);
-		}
-	}
-	
-	public StockCacao() {
-		this.stockcacao = 0.;
+	@Override
+	public void MiseAJourStockLivraison() {
+		// TODO Auto-generated method stub
+		
 	}
 
-	public double CoutStockCacao() {
-		return Constante.COUT_STOCK_UNITAIRE*this.getStockcacao();
+	@Override
+	public void MiseAJourStockTransformation() {
+		// TODO Auto-generated method stub
+		
 	}
+
 }
