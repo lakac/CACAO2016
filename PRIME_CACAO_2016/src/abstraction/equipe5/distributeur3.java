7e8717089411835d982613adab401137f996a2d4
May-linh <may.linh@live.fr>
2016-06-17 14:32:40 +0200

    distributeur 3

--- PRIME_CACAO_2016/src/abstraction/equipe5/distributeur3.java
+++ PRIME_CACAO_2016/src/abstraction/equipe5/distributeur3.java
@@ -3,8 +3,13 @@ package abstraction.equipe5;
 import java.util.ArrayList;
 import java.util.List;
 
+import abstraction.commun.Catalogue;
 import abstraction.commun.CommandeDistri;
 import abstraction.commun.IDistributeur;
+import abstraction.commun.ITransformateurD;
+import abstraction.commun.MarcheDistributeur;
+import abstraction.commun.Produit;
+import abstraction.fourni.Acteur;
 
 public class distributeur3{
 	
@@ -14,13 +19,48 @@ public class distributeur3{
 	private List<CommandeDistri> commande;
 	private Lindt lindt;
 	private VenteDist venteDist;
-	
+	private IDistributeur distributeurRestant;
 		
+	class DistributeurRestant implements Acteur, IDistributeur {
+		public void next() {
+	}
+
+		@Override
+		public String getNom() {
+			// TODO Auto-generated method stub
+			return "Ditributeur restant de Lindt";
+		}
+
+		@Override
+		public List<CommandeDistri> Demande(ITransformateurD t, Catalogue c) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public List<CommandeDistri> ContreDemande(List<CommandeDistri> nouvelle, List<CommandeDistri> ancienne) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Double getStock(Produit p) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Double getPrixVente(Produit p) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	}
 	public distributeur3(int quantite, Lindt lindt, VenteDist venteDist) {
 		this.quantite = quantite;
 		this.commande = new ArrayList<CommandeDistri>();
 		this.lindt = lindt;
 		this.venteDist=venteDist;
+		this.distributeurRestant= new DistributeurRestant();
 	}
 	
 	// prendre les commandes finales de leclerc et carrefour et les multiplier par 3 pour obtenir les commandes du 3eme distributeur.
@@ -33,17 +73,15 @@ public class distributeur3{
 		for(IDistributeur d: lindt.getDistributeurs()){ 
 			for (CommandeDistri cd : MarcheDistributeur.LE_MARCHE_DISTRIBUTEUR.obtenirCommandeFinale(lindt,d)){// commandes finales de leclerc et carrefour
 				commandesResteDuMonde.add(cd);
-				commandesResteDuMonde.get(i).setAcheteur(distributeur3);
-				//il faut créer le distributeur restant (reste du monde) dans l'interface IDistributeur
+				commandesResteDuMonde.get(i).setAcheteur(distributeurRestant);
+				
 				double qteCommandeResteDuMonde=ratioCommandeResteDuMonde*cd.getQuantite();//quantité que le distributeur restant doit commander
 				commandesResteDuMonde.get(i).setQuantite(qteCommandeResteDuMonde); //mise à jour de la bonne quantité dans la commande
 				i++;
 			}
 		}
-		///venteDist.QuantiteDemandeeProduit(commandesResteDuMonde); //Possibilité de l'utilisé pour regrouper toutes les commandes en 1 commande par produit
 		return commandesResteDuMonde;
 	}
-	
 	
 	
 	public Lindt getLindt() {
@@ -63,3 +101,4 @@ public class distributeur3{
 	}
 	
 }
+
