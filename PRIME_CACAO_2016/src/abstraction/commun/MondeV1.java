792ee292720a2af9bc1fd71b0a845c026459b0cf
Alexandre MARTY <alexandre.marty@ymail.com>
2016-06-27 16:40:52 +0200

    27/06

--- PRIME_CACAO_2016/src/abstraction/commun/MondeV1.java
+++ PRIME_CACAO_2016/src/abstraction/commun/MondeV1.java
@@ -64,6 +64,8 @@ public class MondeV1 extends Monde {
 		this.ajouterActeur(p1);
 		abstraction.equipe4.Producteur p2 = new abstraction.equipe4.Producteur(Monde.LE_MONDE);
 		this.ajouterActeur(p2);
+		CotedIvoire CoteDIvoire = new CotedIvoire();
+		this.ajouterActeur(CoteDIvoire);
 		
 		// Marché Producteur
 				MarcheProd marcheProducteur = new MarcheProd();
@@ -108,7 +110,7 @@ public class MondeV1 extends Monde {
 		marcheProducteur.AjoutTransformateur(nestle);
 		marcheProducteur.AjoutTransformateur(lindt);
 		marcheProducteur.AjoutTransformateur(t3);
-		//penser a ajouter la cote d'ivoire
+		marcheProducteur.AjoutProducteur(CoteDIvoire);
 
 		
 
